9714408d149149a832c588332dce8325542cbfa7
DaniMori <danivmorillo@gmail.com>
2023-05-25 17:05:30 +0200

    Versions commit example

---
+++ www/Analyses.R
@@ -0,0 +1,17 @@
+# Libraries:
+library(tidyverse)
+library(countrycode)
+
+# Load data:
+dataset <- read_csv2("www/some_data.csv")
+
+# Compute variables:
+countries <- dataset |>
+  separate_rows(countries, sep = ', ') |>
+  select(id, countries)
+
+# Explore age range:
+dataset |> summarize(
+  age_min = min(age.min),
+  age_max = max(age.max)
+)
