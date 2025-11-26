8e3e6d34a84c206eba0a4e126e77a66a9e88ff8a
DaniMori <DaniMori@users.noreply.github.com>
2025-11-26 21:38:02 +0100

    Revert "Deletes superfluous computation of dataset"
    This reverts commit 286be6e8b35ba2026580b1c3e79f9ca4b1c75530.

--- src/Analyses.R
+++ src/Analyses.R
@@ -10,6 +10,11 @@ dataset <- read_csv2("dat/some_data.csv")
 # Explore dataset:
 dataset
 
+# Compute variables:
+countries <- dataset |>
+  separate_rows(countries, sep = ', ') |>
+  select(id, countries)
+
 # Explore age range:
 dataset |> summarize(
   age_min = min(age.min),
